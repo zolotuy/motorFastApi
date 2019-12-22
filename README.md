ccffab2cbc1e1d051d9db480d1a1223fb9459960
zolotuy <sanyaserebro17@gmail.com>
2019-12-22 14:59:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Library FastAPI
+1. Clone repository
+2. Instal fastAPI: pip install fastapi
+3. Instal uvicorn: pip install uvicorn
+4. Run app: uvicorn app:app --reload
+5. Got to: http://127.0.0.1:8000/docs
+6. Enjoy
---
+++ app.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from salon import Salon
+
+
+class Car(BaseModel):
+    id: int
+    name: str
+    speed: int
+
+
+app = FastAPI()
+
+salon = Salon()
+
+
+@app.get('/')
+def get_all_cars():
+    return salon.get_all_cars()
+
+
+@app.get('/{name}')
+def get_car_by_name(name: str):
+    car = salon.get_car_by_name(name)
+    if car is not None:
+        return car
+    else:
+        return "There is no car with name: " + name
+
+
+@app.post('/addcar')
+def add_car(car: Car):
+    name = car.name
+    speed = car.speed
+    return salon.add_car(name, speed)
+
+
+@app.put('/updatecar')
+def update_car(car: Car):
+    updated_car = salon.update_car(car.id, car.name, car.speed)
+    if updated_car is not None:
+        return updated_car
+    else:
+        return "There is no car with id: " + str(car.id)
+
+
+@app.delete('/deletecar/{id}')
+def delete_car(id: int):
+    is_deleted = salon.delete_car_by_id(id)
+    if is_deleted:
+        return "car with id = " + str(id) + " was successfully deleted"
+    else:
+        return "There is no car with id: " + str(id)
